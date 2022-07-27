f0959bd15c437c029f55f54562c1a83ac32f5910
Бабкина <babkinaolga2000@gmail.com>
2022-07-27 18:48:06 +0300

    refactoring index and even

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,25 +5,27 @@ import {
 } from '../utils.js';
 import { initGame } from '../index.js';
 
-export const startProgressionGame = () => {
-  let lengthArray = getRandomArbitrary(5, 10);
-  let counter = getRandomNumber(5);
-  let start = getRandomNumber(20);
-  let array = generateArr(counter, lengthArray, start);
-  let delIndex = getRandomNumber(lengthArray);
-  let answer = array[delIndex];
-  array[delIndex] = '..';
-  let input = getUserAnswer(array.join(' '));
-  return { answer, input };
-};
 const generateArr = (counter, lengthArray, start) => {
-  let array = [];
+  const array = [];
   array.push(start);
   for (let i = 1; i <= lengthArray; i += 1) {
     array.push(array[i - 1] + counter);
   }
   return array;
 };
+
+export const startProgressionGame = () => {
+  const lengthArray = getRandomArbitrary(5, 10);
+  const counter = getRandomNumber(5);
+  const start = getRandomNumber(20);
+  const array = generateArr(counter, lengthArray, start);
+  const delIndex = getRandomNumber(lengthArray);
+  const answer = array[delIndex];
+  array[delIndex] = '..';
+  const input = getUserAnswer(array.join(' '));
+  return { answer, input };
+};
+
 export const launch = () => {
   initGame(startProgressionGame, 'What number is missing in the progression?');
 };
